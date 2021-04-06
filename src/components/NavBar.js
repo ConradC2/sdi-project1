b48ba6995cce9a0accdd8a7168dab04c9b321ee3
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-04-06 16:07:07 -0500

    initial tests for navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+
+function NavBar() {
+
+    return(
+        <div>
+         <h1 className='navbar-header'>Watch This</h1>
+         <nav>
+          <ul>
+           <li>Home</li>
+           <li>Watch List </li>
+           <li>About</li> 
+          </ul>
+         </nav>
+        </div>
+    )
+};
+
+export default NavBar;
