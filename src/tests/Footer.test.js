741e8f13eda189d6e7e9c62174523d8ad9cbb98c
SmoothE21 <79542682+SmoothE21@users.noreply.github.com>
2021-04-07 12:55:47 -0400

    Update Footer.test.js

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -3,7 +3,7 @@ import App from './../App';
 
 
 
-test('Renders a footer with the text Link to the Movie Database ', () => {
+test('Renders a footer with the text "Link to the Movie Database" ', () => {
   render(<Footer />);
   const HomeElement = screen.getByText(/Link to The Movie Database/i);
   expect(HomeElement).toBeInTheDocument();
