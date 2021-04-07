468e57d5a684ba2eab7ce195695c206c0852aa79
SmoothE21 <79542682+SmoothE21@users.noreply.github.com>
2021-04-07 12:49:52 -0400

    Capitalized the 'f' in footer import file path.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 
 function App() {
   return (
