773ee09eae78c3ddf7c0971ee7fa10dc4f0e890b
talmadge704 <talmadge.bookertjr@gmail.com>
2021-04-07 11:14:22 -0400

    Co-authored-by: ConradC2 <ConradC2@users.noreply.github.com>

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,37 @@
+import TMBD from './images';
+
 function About (){
+    return (
+        <div>
+            <header>ABOUT THE TEAM</header>
+            <article>
+                <h1>04-SDI Student:  Derek Tissaw</h1>
+                <p></p>
+            </article>
+            <article>
+                <h1>04-SDI Student: Talmadge Bookert Jr</h1>
+                <p>Talmadge is an active duty Army E-4 Specialist. His MOS is 25Uniform Signal Support Specialist, where he is very skilled in installing an operating the JCRs, wide variety of radio sets, and the put up and take down of long range antenennas.</p>
+            </article>
+            <article>
+                <h1>04-SDI Student: Antoine Davis</h1>
+                <p></p>
+            </article>
+            <article>
+                <h1>04-SDI Student: Curtis Conrad</h1>
+                <p>Curtis is an active duty Air Force Senior Non-Commissioned Officer.  He serves as a Knowledge manager providing the framework for capturing, harnessing and exploiting intellectual capital, thus improving upon individual and organizational performance through the use of knowledge management practices and collaborative technologies. He is using design thinking and applying it to improve organizational and individual performance and efficiencies of knowledge intensive processes, activities, and tasks.  He is passionate about leveraging today's technology to close knowledge gaps within organizations.
+
+                Connect with him on Linkedin</p>
+            </article>
+            <article>
+                <h1>About the The Movie Database (TMBD) API</h1>
+                <img src="./images/TMBD-image.svg" alt="Primary full blue TMDB logo"></img>
+                <p>The Movie Database (TMDb) is a community built movie and TV database. Every piece of data has been added by our amazing community dating back to 2008. TMDb's strong international focus and breadth of data is largely unmatched and something we're incredibly proud of. Put simply, we live and breathe community and that's precisely what makes us different.</p>
+            </article>
+            
+        </div>
+    )
 
 }
 export default About;
+
+
