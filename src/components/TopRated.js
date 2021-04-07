27a226599e888eb10ea9c81c62e9c36353459418
Curtis Conrad <conradc5749@yahoo.com64824279+ConradC2@users.noreply.github.com>
2021-04-07 14:49:52 -0400

    implemented fetch request for toprated

--- src/components/TopRated.js
+++ src/components/TopRated.js
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+
 function TopRated() {
 
+    const url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=f9a1ac0ab4f944bb3c103d5dc0e9fc4d&language=en-US&page=1';
+    const IMGPATH = "https://image.tmdb.org/t/p/w1280"
+
+    const [ topRated, setTopRated] = useState([])
+
+    useEffect(() => {
+
+        const movieElements = fetch(url)
+        .then(response => response.json())
+        .then(response => setTopRated(response.results)) 
+    })
+    
     return (
         <section role='ratedGrid'>
             <h2>Top Rated</h2>
+            {topRated.map((movie) => {
+                return (
+                    <div>
+                        <h3>{movie.title}</h3>
+                        <img src={`${IMGPATH}/${movie.poster_path}`} />
+                    </div>
+                )
+            })}
         </section>
     )
 }
